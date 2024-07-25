d70a9b71d76e7cf054d3a7d9e9eb7c229c6b2cc3
UmairKhan34 <136227039+UmairKhan34@users.noreply.github.com>
2024-07-25 13:36:50 +0500

    MERN-19 07/25/24 adding new files related to objects

---
+++ object.js
@@ -0,0 +1,52 @@
+const person1 = {
+  name: "Laiba",
+  age: 25,
+  gender: "Female",
+  address: {
+    city: "New York",
+  },
+  array: [2, 2],
+  sayHello: function () {
+    return "Hello Ahmed";
+  },
+};
+const person2 = {
+  name: "Amir",
+  age: 25,
+  gender: "Male",
+  address: {
+    city: "Shujabad",
+  },
+  array: [2, 2],
+  sayHello: function () {
+    return "Hello Amir";
+  },
+};
+const person3 = {
+  name: "Umair",
+  age: 25,
+  gender: "Male",
+  address: {
+    city: "Multan",
+  },
+  array: [2, 2],
+  sayHello: function () {
+    return "Hello umair";
+  },
+};
+const person4 = {
+  name: "Jafar",
+  age: 25,
+  gender: "Male",
+  address: {
+    city: "New York",
+  },
+  array: [2, 2],
+  sayHello: function () {
+    return "Hello jafra";
+  },
+};
+const array = [person1, person2, person3, person4];
+console.log(person1.gender);
+console.log(person1.array[1]);
+console.log(person1.address.city);
